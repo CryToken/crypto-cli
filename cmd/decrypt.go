2bc6fc3206d03a494a6173eabfa93e199ee2a5aa
Goga Token <gogibugo@gmail.com>
2024-07-08 13:14:06 +0000

    Add Decrypt Aes func

--- cmd/decrypt.go
+++ cmd/decrypt.go
@@ -1,13 +1,13 @@
-/*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
 	"fmt"
 
+	"github.com/crytoken/crypto-cli/internal/decrypt"
 	"github.com/spf13/cobra"
 )
+
+var decryptCfg *decrypt.Config
 
 // decryptCmd represents the decrypt command
 var decryptCmd = &cobra.Command{
@@ -15,14 +15,23 @@ var decryptCmd = &cobra.Command{
 	Short: "decrypt file",
 	Long:  `You can decrypt encrypted files just use --file and --key.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("decrypt called")
+		fmt.Println(decryptCfg)
+		decrypt.Run(decryptCfg)
 	},
 }
 
 func init() {
+	//Init Decrypt confing .
+	decryptCfg = decrypt.InitCfg()
 	rootCmd.AddCommand(decryptCmd)
 
 	// Here you will define your flags and configuration settings.
+
+	decryptCmd.Flags().StringVarP(&decryptCfg.Method, "algo", "a", "AES", "CHhooce algorithm to encrypt File")
+	decryptCmd.Flags().StringVarP(&decryptCfg.MethodMode, "Mode", "M", "CFB", "Mode for Algorithm")
+	decryptCmd.Flags().StringVarP(&decryptCfg.Key, "key", "k", "", "Key for cipher")
+	decryptCmd.Flags().StringVarP(&decryptCfg.OutputFile, "out", "o", "", "path to output file")
+	decryptCmd.Flags().StringVarP(&decryptCfg.InputFile, "file", "f", "", "File to encrypt")
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
---
+++ internal/decrypt/decrypt.go
@@ -0,0 +1,67 @@
+package decrypt
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"errors"
+	"fmt"
+	"io/ioutil"
+)
+
+func Run(cfg *Config) {
+	err := parseCfg(cfg)
+	if err != nil {
+		fmt.Println("Decrypting error:", err)
+	}
+
+	err = decryptFile(cfg)
+	if err != nil {
+		fmt.Println("Somthing wrong", err)
+	}
+
+}
+
+func decryptFile(cfg *Config) error {
+	switch cfg.Method {
+	case "AES":
+		err := decryptAes(cfg)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func decryptAes(cfg *Config) error {
+	switch cfg.MethodMode {
+	case "CFB":
+		err := decryptAesCFB(cfg)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func decryptAesCFB(cfg *Config) error {
+	cipherFile, err := ioutil.ReadFile(cfg.InputFile)
+	if err != nil {
+		return errors.New("reading cipherFile err")
+	}
+
+	block, err := aes.NewCipher(cfg.KeyHash)
+	if err != nil {
+		return errors.New("err to create block")
+	}
+	iv := cipherFile[:aes.BlockSize]
+	cipherFile = cipherFile[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+	stream.XORKeyStream(cipherFile, cipherFile)
+
+	err = ioutil.WriteFile(cfg.OutputFile, cipherFile, 0644)
+	if err != nil {
+		return errors.New("err to write to file")
+	}
+	return nil
+}
