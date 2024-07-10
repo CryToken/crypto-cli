2010deb4e662f39e65bddc03e203ea2d8ac16e78
Goga Token <gogibugo@gmail.com>
2024-07-10 04:37:27 +0000

    Add File sha256 handler

--- internal/hash/hashFuncs.go
+++ internal/hash/hashFuncs.go
@@ -2,7 +2,9 @@ package hash
 
 import (
 	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/crytoken/consl"
@@ -32,6 +34,11 @@ func sha256router(cfg *HashConfig) error {
 		if err != nil {
 			return err
 		}
+	case "File":
+		err := sha256File(cfg)
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
@@ -41,7 +48,27 @@ func sha256Text(cfg *HashConfig) error {
 	hasher := sha256.New()
 	hasher.Write(data)
 	result := fmt.Sprintf("%x", hasher.Sum(nil))
-	consl.PrintBlue("SHA256:\n")
+	consl.PrintBlue("SHA-256:\n")
 	fmt.Printf("%s\n", result)
+	return nil
+}
+
+func sha256File(cfg *HashConfig) error {
+	//Read file and close befor quit
+	file, err := os.Open(cfg.InputFile)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	hash := sha256.New()
+	if _, err = io.Copy(hash, file); err != nil {
+		return err
+	}
+	hashBytes := hash.Sum(nil)
+	hashString := hex.EncodeToString(hashBytes)
+	consl.PrintBlue("SHA-256 File Hash:\n")
+	fmt.Println(hashString)
 	return nil
 }
